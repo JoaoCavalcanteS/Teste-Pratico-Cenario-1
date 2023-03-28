0b6be38a578181ad369c08855cfaed316d1f12c3
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2023-03-27 21:08:59 -0300

    Correção Semântica

--- Blog/JS/blog.js
+++ Blog/JS/blog.js
@@ -6,52 +6,52 @@ function buscarPosts() {
     const urlPosts = 'https://jsonplaceholder.typicode.com/posts';
 
     fetch(urlPosts)
-    .then(async response => {
-        const data = await response.json();//Resultado Assincrono para esperar o resultado completo, para dar o resultado já pronto
-        const listaPosts = document.getElementById('lista');
+        .then(async response => {
+            const data = await response.json();//Resultado Assincrono para esperar o resultado completo, para dar o resultado já pronto
+            const listaPosts = document.getElementById('lista');
 
-        for (let i = 0; i < data.length; i++) {
-            let post = data[i];
-            let itemPost = document.createElement('div');
-            itemPost.className =  'list-group-item list-group-item-action';
-            itemPost.innerText = post.id + "-" + post.title;
-            itemPost.addEventListener('click', ()=>{ 
-                
-                //function buscar comentários
-                const postId = post.id;
+            for (let i = 0; i < data.length; i++) {
+                let post = data[i];
+                let itemPost = document.createElement('div');
+                itemPost.className = 'list-group-item list-group-item-action';
+                itemPost.innerText = post.id + "-" + post.title;
+                itemPost.addEventListener('click', () => {
 
-                //se o id de todos os posts nao contem isso, entao recebe uma mensagem de erro
-                if (!idposts.includes(parseInt(postId))) {
-                    const listaComentarios = document.getElementById('listagemComentarios');
-                    listaComentarios.innerHTML = 'Este comentário não existe';
-                    
-                } else {
-                    const urlComentarios = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
-                    fetch(urlComentarios)
-                    .then(async response => {
-                        const data = await response.json();
-            
+                    //function buscar comentários
+                    const postId = post.id;
+
+                    //se o id de todos os posts nao contem isso, entao recebe uma mensagem de erro
+                    if (!idposts.includes(parseInt(postId))) {
                         const listaComentarios = document.getElementById('listagemComentarios');
-                        listaComentarios.innerHTML = '';
-            
-                        //for OF
-                        for (const comentario of data) {
-                            const itemComentario = document.createElement('div');
-                            itemComentario.innerText = "Comentário: " + comentario.body+".";
-                            listaComentarios.appendChild(itemComentario);
-                        }
-            
-                    }).catch(erro => {
-                        let listaComentarios = document.getElementById('listagemComentarios');
-                        listaComentarios.innerHTML = "Erro: " + erro.message;
-                    });
-                }
-            })
-            listaPosts.appendChild(itemPost);
-            idposts.push(post.id);
-        }
+                        listaComentarios.innerHTML = 'Este comentário não existe';
 
-    }).catch(erro => alert(erro.message));
+                    } else {
+                        const urlComentarios = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
+                        fetch(urlComentarios)
+                            .then(async response => {
+                                const data = await response.json();
+
+                                const listaComentarios = document.getElementById('listagemComentarios');
+                                listaComentarios.innerHTML = '';
+
+                                //for OF
+                                for (const comentario of data) {
+                                    const itemComentario = document.createElement('div');
+                                    itemComentario.innerText = "Comentário: " + comentario.body + ".";
+                                    listaComentarios.appendChild(itemComentario);
+                                }
+
+                            }).catch(erro => {
+                                let listaComentarios = document.getElementById('listagemComentarios');
+                                listaComentarios.innerHTML = "Erro: " + erro.message;
+                            });
+                    }
+                })
+                listaPosts.appendChild(itemPost);
+                idposts.push(post.id);
+            }
+
+        }).catch(erro => alert(erro.message));
 
 }
 
@@ -61,18 +61,18 @@ function buscarUsuarios() {
     const urlUsuarios = 'https://jsonplaceholder.typicode.com/users';
 
     fetch(urlUsuarios)
-    .then(async response => {
-        const data = await response.json();
+        .then(async response => {
+            const data = await response.json();
 
-        const listaUsuarios = document.getElementById('listagemUsuarios');
+            const listaUsuarios = document.getElementById('listagemUsuarios');
 
-        for (const usuario of data) {
-            const itemUsuario = document.createElement('li');
-            itemUsuario.innerText = usuario.name;
-            listaUsuarios.appendChild(itemUsuario);
-        }
+            for (const usuario of data) {
+                const itemUsuario = document.createElement('li');
+                itemUsuario.innerText = usuario.name;
+                listaUsuarios.appendChild(itemUsuario);
+            }
 
-    });
+        });
 
 }
 
@@ -83,47 +83,47 @@ function buscarUsuario() {
     const urlUsuario = `https://jsonplaceholder.typicode.com/users/${userId}`;
 
     fetch(urlUsuario)
-    .then(async response => {
-        const data = await response.json();
-        const detalhesUsuario = document.getElementById('detalhesUsuario');
-        detalhesUsuario.innerHTML = '';
+        .then(async response => {
+            const data = await response.json();
+            const detalhesUsuario = document.getElementById('detalhesUsuario');
+            detalhesUsuario.innerHTML = '';
 
-        if (response.status === 200) {
+            if (response.status === 200) {
+                const nomeUsuario = document.createElement('h2');
+                nomeUsuario.innerText = data.name;
+                detalhesUsuario.appendChild(nomeUsuario);
+
+                const emailUsuario = document.createElement('p');
+                emailUsuario.innerText = `Email: ${data.email}`;
+                detalhesUsuario.appendChild(emailUsuario);
+
+                const enderecoUsuario = document.createElement('p');
+                enderecoUsuario.innerText = `Endereço: ${data.address.street}, ${data.address.suite}, ${data.address.city}`;
+                detalhesUsuario.appendChild(enderecoUsuario);
+
+            } else if (response.status === 404) {
+                const nomeUsuario = document.createElement('h2');
+                nomeUsuario.innerText = "Este usuário não existe";
+                detalhesUsuario.appendChild(nomeUsuario);
+
+            } else {
+                const nomeUsuario = document.createElement('h2');
+                nomeUsuario.innerText = "Erro " + response.status;
+                detalhesUsuario.appendChild(nomeUsuario);
+
+            }
+        }).catch(erro => {
+            const detalhesUsuario = document.getElementById('detalhesUsuario');
+            detalhesUsuario.innerHTML = '';
+
             const nomeUsuario = document.createElement('h2');
-            nomeUsuario.innerText = data.name;
+            nomeUsuario.innerText = "Erro: " + erro.message;
             detalhesUsuario.appendChild(nomeUsuario);
-
-            const emailUsuario = document.createElement('p');
-            emailUsuario.innerText = `Email: ${data.email}`;
-            detalhesUsuario.appendChild(emailUsuario);
-
-            const enderecoUsuario = document.createElement('p');
-            enderecoUsuario.innerText = `Endereço: ${data.address.street}, ${data.address.suite}, ${data.address.city}`;
-            detalhesUsuario.appendChild(enderecoUsuario);
-
-        } else if (response.status === 404) {
-            const nomeUsuario = document.createElement('h2');
-            nomeUsuario.innerText = "Este usuário não existe";
-            detalhesUsuario.appendChild(nomeUsuario);
-
-        } else {
-            const nomeUsuario = document.createElement('h2');
-            nomeUsuario.innerText = "Erro " + response.status;
-            detalhesUsuario.appendChild(nomeUsuario);
-
-        }
-    }).catch(erro => {
-        const detalhesUsuario = document.getElementById('detalhesUsuario');
-        detalhesUsuario.innerHTML = '';
-
-        const nomeUsuario = document.createElement('h2');
-        nomeUsuario.innerText = "Erro: " + erro.message;
-        detalhesUsuario.appendChild(nomeUsuario);
-    });
+        });
 
 }
 
-window.onload = function() {
+window.onload = function () {
     buscarPosts();
     buscarUsuarios();
 };
